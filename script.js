ee166e4ef36975be74c01c313c0f2f556422b4c7
Caleb Tseng <calebtseng07@gmail.com>
2023-04-04 04:30:40 -0700

    Give function to display player choice on gameBoard

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
+const playerX = "X"
+const playerO = "O"
+const boardElements = document.querySelectorAll('.board');
+let oTurn
+
+let currentPlayer = playerX
+
+boardElements.forEach(cell => {
+    cell.addEventListener('click', handleClick, { once: true })
+});
+
+function handleClick(e) {
+    const id = e.target.id
+
+    if (!boardElements[id]) {
+        boardElements[id] = currentPlayer;
+        e.target.innerText = currentPlayer;
+
+        currentPlayer = currentPlayer == playerX ? playerO : playerX;
+    }
+    // const currentClass = oTurn ? o_class : x_class;
+    // placeTurn(cell, currentClass);
+    // Check for win
+    // check for draw
+    //switch turn
+}
+// handleClick()
+
+// function placeTurn(cell, currentClass) {
+//     cell.classList.add(currentClass)
+// }
+
 /* RESTART BUTTON */
 
 let restart = document.querySelector(".gameRestart");
