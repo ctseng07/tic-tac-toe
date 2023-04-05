b9c54ecc1e8f2f7c7824e8364bf6b28139ccfe37
Caleb Tseng <calebtseng07@gmail.com>
2023-04-05 04:55:36 -0700

    Attempt to create function to look for winner

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 const playerX = "X"
 const playerO = "O"
-const boardElements = document.querySelectorAll('.board');
-let oTurn
-
 let currentPlayer = playerX
+const boardElements = document.querySelectorAll('.board');
 
-boardElements.forEach(cell => {
-    cell.addEventListener('click', handleClick, { once: true })
-});
+const winningCombinations = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 4, 6],
+    [2, 5, 8],
+    [3, 4, 5],
+    [6, 7, 8],
+];
+
+function startGame() {
+    boardElements.forEach(cell => {
+        cell.addEventListener('click', handleClick, { once: true })
+    });
+
+}
 
 function handleClick(e) {
     const id = e.target.id
@@ -18,13 +30,25 @@ function handleClick(e) {
 
         currentPlayer = currentPlayer == playerX ? playerO : playerX;
     }
+    if (checkWin(currentPlayer)) {
+        console.log("winner");
+    }
     // const currentClass = oTurn ? o_class : x_class;
     // placeTurn(cell, currentClass);
     // Check for win
     // check for draw
     //switch turn
 }
-// handleClick()
+
+function checkWin(currentPlayer) {
+    return winningCombinations.some(combination => {
+        return combination.every(index => {
+            return boardElements[index].classList.contains(currentPlayer)
+        })
+    })
+}
+
+startGame();
 
 // function placeTurn(cell, currentClass) {
 //     cell.classList.add(currentClass)
@@ -35,7 +59,13 @@ function handleClick(e) {
 let restart = document.querySelector(".gameRestart");
 
 function restartBtn() {
-    window.location.reload(true);
+    boardElements.fill(null)
+
+    cell.forEach(cell => {
+        cell.innerText = "";
+    });
+
+    currentPlayer = playerX;
 }
 
 restart.addEventListener("click", restartBtn);
