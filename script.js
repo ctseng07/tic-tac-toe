957fd9c1598ccdd8f11af21bae0fc9da21de461d
Caleb Tseng <calebtseng07@gmail.com>
2023-04-11 03:59:32 -0700

    Attempt to change background color for each turn

--- script.js
+++ script.js
@@ -2,12 +2,16 @@
 const gameBoard = document.querySelectorAll(".board");
 const restartBtn = document.querySelector(".gameRestart");
 let message = document.querySelector(".winning-message");
+let playerX = "X";
+let playerO = "O";
+let gamesPlayed = 0;
 
-let currentPlayer = "X";
+let currentPlayer = playerX;
 let moves = 0;
 let gameOver = false;
 let teamX = 0;
 let teamO = 0;
+let backgroundColor = "aaa";
 
 // function to handle player moves
 function handleClick(e) {
@@ -15,6 +19,7 @@ function handleClick(e) {
     if (e.target.textContent !== "") return;
 
     e.target.textContent = currentPlayer;
+    e.target.backgroundColor = backgroundColor;
     moves++;
 
     if (checkForWinner()) {
@@ -27,7 +32,8 @@ function handleClick(e) {
         message.textContent = "It's A Tie!";
         restart();
     } else {
-        currentPlayer = currentPlayer === "X" ? "O" : "X";
+        currentPlayer = currentPlayer === playerX ? playerO : playerX;
+        backgroundColor = backgroundColor === "#282727" ? "#8b0000" : "#282727";
     }
 }
 
@@ -52,13 +58,14 @@ function startGame() {
     gameBoard.forEach((square) => {
         square.addEventListener("click", handleClick);
     });
+    // playerX.style.backgroundColor = "#282727"
 };
 
 startGame();
 
 // function to display score
 function updateScore() {
-    if (currentPlayer === "X") {
+    if (currentPlayer === playerX) {
         teamX++;
         document.getElementById('player').innerText = teamX;
     } else {
@@ -73,7 +80,7 @@ function restart() {
         gameBoard.forEach((square) => {
             square.textContent = "";
         });
-        currentPlayer = "X";
+        currentPlayer = playerX;
         moves = 0;
         gameOver = false;
         message.textContent = "";
@@ -81,137 +88,14 @@ function restart() {
 }
 
 restartBtn.addEventListener("click", restart);
-// const playerX = "X"
-// const playerO = "O"
-// let currentPlayer = playerX
-// // const boardElements = document.querySelectorAll('.board');
-
-// //defining winning combinations
-
-// const winningCombinations = [
-//     [0, 1, 2], [3, 4, 5], [6, 7, 8], // rows
-//     [0, 3, 6], [1, 4, 7], [2, 5, 8], // columns
-//     [0, 4, 8], [2, 4, 6] // diagonals
-// ];
-
-// function startGame() {
-//     boardElements.forEach(cell => {
-//         cell.addEventListener('click', boxClicked, { once: true })
-//     });
-// };
-// // switch turn
-// function boxClicked(e) {
-//     const id = e.target.id
-
-//     if (!boardElements[id]) {
-//         boardElements[id] = currentPlayer;
-//         e.target.innerText = currentPlayer;
-
-//         currentPlayer = currentPlayer == playerX ? playerO : playerX;
-//     }
-//     // Check for win
-//     if (checkWin(currentPlayer)) {
-//     }
-
-// }
-
-// function checkWin(currentPlayer) {
-//     return winningCombinations.some(combination => {
-//         return combination.every(index => {
-//             return boardElements[index].classList.contains(currentPlayer)
-//         })
-//     })
-// }
 
 
-// function endGame(draw) {
-//     if (draw) {
-//         winningMessageTextElement.innerText = 'Draw!'
-//     } else {
-//         winningMessageTextElement.innerText = `${circleTurn ? "O's" : "X's"} Wins!`
-//     }
-//     winningMessageElement.classList.add('show')
-// }
-// // console.log(checkWin("currentPlayer"));
-// // checkWin(currentPlayer);
-// // check for draw
-// // add home page
-// //2 options pvp / pvc
-// // give function to enter name of player
-// startGame();
-
-// /* RESTART BUTTON */
-
-// let restart = document.querySelector(".gameRestart");
-
-// function restartBtn(cell) {
-//     boardElements.fill(null)
-
-//     cell.forEach(cell => {
-//         cell.innerText = "";
-//     });
-
-//     currentPlayer = playerX;
-// }
-
-// restart.addEventListener("click", restartBtn);
-
-//initialize game variables
-// let currentPlayer = 'X';
-// let gameOver = false;
-// let board = ['', '', '', '', '', '', '', '', ''];
-
-// // function to check for a win
-// function checkForWin() {
-//     for (let i = 0; i < winningCombinations.length; i++) {
-//         const [a, b, c] = winningCombinations[i];
-//         if (board[a] && board[a] === board[b] && board[a] === board[c]) {
-//             return true;
-//         }
-//     }
-//     return false
-// }
-
-// // function to handle player moves
-// function handlePlayerMove(cell) {
-//     if (gameOver || board[cell]) {
-//         return;
-//     }
-//     board[cell] = currentPlayer;
-//     document.querySelectorAll('box').innerText = currentPlayer;
-//     if (checkForWin()) {
-//         document.getElementById('winningMessage').innerText = currentPlayer + 'wins!';
-//         gameOver = true;
-//     } else {
-//         currentPlayer = currentPlayer === playerX ? playerO : playerX;
-//     }
-//     console.log(currentPlayer);
-// }
-
-// function startGame() {
-//     cells.forEach(cell => {
-//         cell.addEventListener('click', boxClicked, { once: true })
-//     });
-// };
-
-// function boxClicked(e) {
-//     const id = e.target.id
-
-//     if (!cells[id]) {
-//         cells[id] = currentPlayer;
-//         e.target.innerText = currentPlayer;
-
-//         currentPlayer = currentPlayer == playerX ? playerO : playerX;
-//     }
-// }
-
-// // add event listeners to each cell
-
-// const cells = document.querySelectorAll('.board');
-// for (let i = 0; i < cells.length; i++) {
-//     cells[i].addEventListener('click', function () {
-//         handlePlayerMove(i);
-//     });
-// }
-
-// startGame();
+// player turn
+function playerTurn(playerX, playerO) {
+    const playerTurn = document.querySelector(".userSelect");
+    if (playerX) {
+        playerTurn.backgroundColor = "#aaa";
+    } else {
+        playerTurn.backgroundColor = "#bbb";
+    }
+}
